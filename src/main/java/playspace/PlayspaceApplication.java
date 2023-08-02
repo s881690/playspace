df3067bccc0d6873b0f13dbf9eaa3d40d432018e
sandy <s88169019@gmail.com>
2023-08-03 00:31:15 +0800

    Initial commit

---
+++ src/main/java/playspace/PlayspaceApplication.java
@@ -0,0 +1,13 @@
+package playspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/playspace/PlayspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package playspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
